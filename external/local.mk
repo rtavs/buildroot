3ad8548eb98a7d352ee6e83fbe7d028ae229f876
Kaspter Ju <nigh0st3018@gmail.com>
2015-05-24 02:42:09 +0800

    timebox: use local srcdir override for develpment
    uboot/kernel/toybox/busybox
    
    Signed-off-by: Kaspter Ju <nigh0st3018@gmail.com>

---
+++ external/local.mk
@@ -0,0 +1,5 @@
+
+UBOOT_OVERRIDE_SRCDIR = /Repos/system/uboot/
+#LINUX_OVERRIDE_SRCDIR = /Repos/system/linux/
+#BUSYBOX_OVERRIDE_SRCDIR = /Repos/system/busybox/
+#TOYBOX_OVERRIDE_SRCDIR = /Repos/system/toybox
