861d1bcf5c81813df8c0492848b55e2185fe983e
Kaspter Ju <nigh0st3018@gmail.com>
2015-05-24 18:57:34 +0800

    timebox: tmp use toybox local override
    Signed-off-by: Kaspter Ju <nigh0st3018@gmail.com>

--- external/local.mk
+++ external/local.mk
@@ -1,5 +1,6 @@
 
 UBOOT_OVERRIDE_SRCDIR = /Repos/system/uboot/
+TOYBOX_OVERRIDE_SRCDIR = /Repos/system/toybox/
 #LINUX_OVERRIDE_SRCDIR = /Repos/system/linux/
 #BUSYBOX_OVERRIDE_SRCDIR = /Repos/system/busybox/
 #TOYBOX_OVERRIDE_SRCDIR = /Repos/system/toybox
