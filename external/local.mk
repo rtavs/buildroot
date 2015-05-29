87afd6ba4e9ce3405be27572f5f3e13808e467f7
Kaspter Ju <nigh0st3018@gmail.com>
2015-05-29 22:34:51 +0800

    timebox: linux: use local override src
    Signed-off-by: Kaspter Ju <nigh0st3018@gmail.com>

--- external/local.mk
+++ external/local.mk
@@ -1,6 +1,6 @@
 
 UBOOT_OVERRIDE_SRCDIR = /Repos/system/uboot/
 TOYBOX_OVERRIDE_SRCDIR = /Repos/system/toybox/
-#LINUX_OVERRIDE_SRCDIR = /Repos/system/linux/
+LINUX_OVERRIDE_SRCDIR = /Repos/system/linux/
 #BUSYBOX_OVERRIDE_SRCDIR = /Repos/system/busybox/
 #TOYBOX_OVERRIDE_SRCDIR = /Repos/system/toybox
